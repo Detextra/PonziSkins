5705ad053d9803c8e36937ff2784f74952a7d2e4
Antonin Boyon <antonin.boyon@gmail.com>
2022-10-07 11:39:40 +0200

    vue.js project base

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const db = require("./items");
 //console.log(db);
 
@@ -8,26 +6,36 @@ const keys = Object.keys(db);
 const randomIndex = Math.floor(Math.random() * keys.length);
 const randomKey = keys[randomIndex];
 
-// getting multiple one
+// pie chart values
+var skinsPie = [{
+    names: [], 
+    skin: [],
+    type: 'pie'
+}];
+
+// getting skins and putting these values into a pieData
 var skins = [];
+
 for (let i=0; i<10; i++) {
     skins.push(db[randomKey]);
+    skinsPie[0]['skin'].push(skins[i]['skin']);
+    skinsPie[0]['names'].push(skins[i]['type']);
 }
 
 console.log(skins.length);
 //console.log(skins);
-console.log(skins[1]['type']+' '+skins[1]['skin']); // to get the type or the skin of an item
-console.log(skins[1]['prices'].filter(function (price) {
+/*console.log(skins[1]['type']+' '+skins[1]['skin']); // to get the type or the skin of an item
+var skin1 = skins[1]['prices'].filter(function (price) {
     return price.seller == "buff163"; // change buff163 by the name of the market
-})); // to get the price of a specific market
+})[0]['price']; // to get the price of a specific market
+console.log(skin1);*/
+
+//console.log(skins[1]);
+
+// Pie chart purpose
+var layout = {
+    height: 400,
+    width: 500
+};
 
 
-// creation of the node server
-let interface = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Ponzi skins\n');
-});
-
-// utilisation of the port 3000
-interface.listen(3000, '127.0.0.1');
-console.log('Server running on http://localhost:3000/');
